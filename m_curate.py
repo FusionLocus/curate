0dd1d0587789110a8e90c76ff6f1f35075fb0bf2
FusionLocus <35812161+FusionLocus@users.noreply.github.com>
2020-06-24 13:17:04 +0100

    Changed f_hdf5analyse
    Changed looping structure to increase efficiency.

--- m_curate.py
+++ m_curate.py
@@ -17,6 +17,8 @@ Description: Gets desired data from localised events obtained from picasso pick 
 Arguments:
     a_wantedprops - (int array) which headings from the picasso data are wanted. Numbered from 1 upwards (not 0 - beware!) - see beginning of m_curate for list of some useful ones.
     v_relpath - (string) relative path from the main project directory.
+Returns:
+    a_props - (array) contains the requested data. Each row is a property, each column corresponds to a given event.
 Version: v0.9
 Dependencies: numpy, scipy, matplotlib, h5py, os.
 Last Updated: 23/06/2020
@@ -35,16 +37,14 @@ def f_hdf5analyse(a_wantedprops,v_relpath=''):
     # Pick a property to find (or pick array of them). Just here to have for redundancy purposes.
     a_props = np.zeros((len(a_wantedprops),v_hd5propslen)) # Preallocation of array for the attributes we want.
     
-    # Iterate through the properties one at a time.
-    idx3 = 0 # Index used to index into a_props in the correct row.
-    for idx1 in a_wantedprops :
-        # Get a copy of each of these properties for each group in the data.
-        # Define an index to loop over.
-        idx2 = 0
-        while idx2 < v_hd5propslen :
+    # Iterate through the events one at a time and get the required properties of each event.
+    idx2 = 0  # Define an index to loop over.
+    while idx2 < v_hd5propslen :
+        idx3 = 0 # Index used to index into a_props in the correct row.
+        for idx1 in a_wantedprops :
             a_props[idx3,idx2] = a_hd5props[idx2][idx1]
-            idx2 += 1
-        idx3 += 1 # Look at the next row of a_props.
+            idx3 += 1 # Look at the next row of a_props.
+        idx2 += 1
 
     a_hd5propsfileobj.close() # Close file object to avoid problems.
     return a_props
@@ -57,6 +57,8 @@ Arguments:
     v_bins - (Int) Number of bins to use. Default: 20.
     v_qname - (String) Name of quantity being plotted. Default: 'x'.
     v_qunit - (String) Unit of quantity being plotted.
+Returns:
+    fig, ax - pyplot objects.
 Dependencies: numpy, scipy, matplotlib.
 Author: Joe Pollacco
 Last Updated: 24/06/2020
@@ -77,3 +79,33 @@ def f_1dhistplotter(a_histdata,v_bins=20,v_qname='x',v_qunit=''):
     
     return fig, ax
 
+
+"""
+Name: f_2dhistplotter
+Description: *Very* simple function to plot a 2D histogram of the desired quantity.
+Arguments:
+    a_histdata1 - (Float Array) 1D numpy array to be turned into histogram.
+    a_histdata2 - (Float Array) 1D numpy array to be turned into histogram.
+    v_bins - (Int) Number of bins to use. Default: 20.
+    v_qname - (String) Name of quantity being plotted. Default: 'x'.
+    v_qunit - (String) Unit of quantity being plotted.
+Returns:
+    fig, ax - pyplot objects.
+Dependencies: numpy, scipy, matplotlib.
+Author: Joe Pollacco
+Last Updated: 24/06/2020
+"""
+
+def f_2dhistplotter(a_histdata,v_bins=20,v_qname='x',v_qunit=''):
+    # Prepaare figure
+    fig, ax = plt.subplots()
+    # Generate Histogram
+    n, bins, patches = plt.hist(a_histdata, v_bins,histtype='step')
+    
+    # Label the histogram
+    plt.xlabel(v_qname + ' (' + v_qunit + ')')
+    plt.ylabel('Counts')
+    plt.title('Histogram showing distribution of ' + v_qname + '.')
+    
+    
+    return fig, ax
