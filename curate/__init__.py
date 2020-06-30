5d4be84d8cbd3cd166bafd291af969078c36f5aa
FusionLocus <35812161+FusionLocus@users.noreply.github.com>
2020-06-30 11:15:31 +0100

    Added classes
    Added classes.py and some useful instances that you can use to quickly refer to properties without having to find out/remember their columns, etc.

--- curate/__init__.py
+++ curate/__init__.py
@@ -4,7 +4,7 @@ Created on Mon Jun 29 11:09:39 2020
 
 @author: Joe
 """
-from curate.plotting import f_1dhistplotter
-from curate.reading import f_hdf5analyse
-from curate.utilities import f_lifetimeget
-from curate.utilities import f_fft
+import curate.plotting 
+import curate.reading 
+import curate.utilities 
+import curate.classes
--- curate/classes.py
+++ curate/classes.py
@@ -2,5 +2,78 @@
 """
 Classes and instance variables available for use.
 """
+class c_PickField:
+    def __init__(self,name,idx,unit):
+        self.name = name
+        self.idx = idx
+        self.unit = unit
+        
+# Instances for localisation data.
+i_frame = c_PickField('frame',0,'')
 
+i_x = c_PickField('x',1,'px')
 
+i_y = c_PickField('y',2,'px')
+
+i_photons = c_PickField('photons',3,'')
+
+i_sx = c_PickField('sx',4,'px')
+
+i_sy = c_PickField('sy',5,'px')
+
+i_bg = c_PickField('bg',6,'')
+
+i_lpx = c_PickField('lpx',7,'px') 
+
+i_lpy = c_PickField('lpy',8,'px')   
+
+i_ellipticity = c_PickField('ellipticity',9,'')        
+
+i_netgradient = c_PickField('net_gradient',10,'')
+
+#l added here to differentiate it from pick properties group
+i_lgroup = c_PickField('group',11,'') 
+
+# Instances for pickproperties data.
+i_pgroup = c_PickField('group',0,'')
+
+i_nevents = c_PickField('n_events',1,'')
+
+i_framemean = c_PickField('frame_mean',2,'')
+
+i_framestd = c_PickField('frame_std',3,'')
+
+i_xmean = c_PickField('x_mean',4,'px')
+
+i_xstd = c_PickField('x_std',5,'px')
+
+i_ymean = c_PickField('y_mean',6,'px')
+
+i_ystd = c_PickField('y_std',7,'px')
+
+i_photonsmean = c_PickField('photons_mean',8,'')
+
+i_photonsstd = c_PickField('photons_std',9,'')
+
+i_sxmean = c_PickField('sx_mean',10,'px')
+
+i_sxstd = c_PickField('sx_std',11,'px')
+
+i_symean = c_PickField('sy_mean',12,'px')
+
+i_systd = c_PickField('sy_std',13,'px')
+
+"""
+Need to finish this.
+i_pgroup = c_PickField('',0,'')
+
+i_pgroup = c_PickField('',0,'')
+
+i_pgroup = c_PickField('',0,'')
+
+i_pgroup = c_PickField('',0,'')
+
+i_pgroup = c_PickField('',0,'')
+
+i_pgroup = c_PickField('',0,'')
+"""
